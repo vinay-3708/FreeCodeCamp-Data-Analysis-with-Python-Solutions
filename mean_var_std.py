e26ca7f19e7786e91a0499a669ff19ffea886601
vinay <vinay588325@gmail.com>
2024-10-20 15:27:33 +0530

    Added Solution

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,16 @@
 import numpy as np
 
 def calculate(list):
-
-
-
-
-    return calculations
+    if len(list) == 9:
+        dic = {}
+        arr = np.array(list)
+        re_arr = arr.reshape((3,3))
+        dic['mean'] = [re_arr.mean(axis=0).tolist(), re_arr.mean(axis=1).tolist(), arr.mean().tolist()]
+        dic['variance'] = [re_arr.var(axis=0).tolist(), re_arr.var(axis=1).tolist(), arr.var().tolist()]
+        dic['standard deviation'] = [re_arr.std(axis=0).tolist(), re_arr.std(axis=1).tolist(), arr.std().tolist()]
+        dic['max'] = [re_arr.max(axis=0).tolist(), re_arr.max(axis=1).tolist(), arr.max().tolist()]
+        dic['min'] = [re_arr.min(axis=0).tolist(), re_arr.min(axis=1).tolist(), arr.min().tolist()]
+        dic['sum'] = [re_arr.sum(axis=0).tolist(), re_arr.sum(axis=1).tolist(), arr.sum().tolist()]
+        return dic   
+    else:
+        raise ValueError("List must contain nine numbers.")
